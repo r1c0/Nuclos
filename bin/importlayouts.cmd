1e9f01ccfe75e2114d687616588e0c1986a134c5
radig <radig@33553225-e83a-40c5-8946-3093c540bf14>
2007-03-24 06:28:54 +0000

    no message
    git-svn-id: https://svn.novabit.de/nov_nucleus/trunk@44 33553225-e83a-40c5-8946-3093c540bf14

---
+++ bin/importlayouts.cmd
@@ -0,0 +1,2 @@
+call run-console.cmd nucleus nucleus -importmasterdatalayouts src\de\novabit\nucleus\client\masterdata\layoutml
+call run-console.cmd nucleus nucleus -importleasedobjectlayouts src\de\novabit\nucleus\client\genericobject\layoutml
