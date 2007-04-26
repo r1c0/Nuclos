a29455d2b968a5405a65f47d746eb9d77bc61522
radig <radig@33553225-e83a-40c5-8946-3093c540bf14>
2007-04-26 06:22:53 +0000

    + username/password
    git-svn-id: https://svn.novabit.de/nov_nucleus/trunk@357 33553225-e83a-40c5-8946-3093c540bf14

--- bin/import-layouts.cmd
+++ bin/import-layouts.cmd
@@ -1,4 +1,9 @@
-set basedir=%~dp0..
+@echo off
+setlocal
 
-call %basedir%\bin\run-console.cmd nucleus nucleus -importmasterdatalayouts %basedir%\src\de\novabit\nucleus\client\masterdata\layoutml
-call %basedir%\bin\run-console.cmd nucleus nucleus -importleasedobjectlayouts %basedir%\src\de\novabit\nucleus\client\genericobject\layoutml
+set basedir=%~dp0..
+set username=%1
+set password=%2
+
+call %basedir%\bin\run-console.cmd %username% %password% -importmasterdatalayouts %basedir%\src\de\novabit\nucleus\client\masterdata\layoutml
+call %basedir%\bin\run-console.cmd %username% %password% -importleasedobjectlayouts %basedir%\src\de\novabit\nucleus\client\genericobject\layoutml
