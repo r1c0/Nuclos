6ac0d19e2d24005716eb2e073f6ddbcc18d51e5c
radig <radig@33553225-e83a-40c5-8946-3093c540bf14>
2007-05-07 07:55:52 +0000

    c adjusted
    git-svn-id: https://svn.novabit.de/nov_nucleus/trunk@461 33553225-e83a-40c5-8946-3093c540bf14

--- bin/drop-nucleus-schema.cmd
+++ bin/drop-nucleus-schema.cmd
@@ -4,7 +4,7 @@ if "%1" == "" goto usage
 set login=%1
 set basedir=%~dp0..
 
-sqlplus %login% @%basedir%\db\utils\drop-nucleus-schema.sql <%basedir%\db\utils\quit
+sqlplus %login% @%basedir%\db\utils\drop-schema.sql <%basedir%\db\utils\quit
 goto end
 
 :usage
