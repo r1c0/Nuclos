3b898de7e67ff90630335b7e4ca774402e60d0ce
radig <radig@33553225-e83a-40c5-8946-3093c540bf14>
2007-05-08 13:16:15 +0000

    c NucleusConsole reads server configuration from nucleus-client-server-config.properties c JNDI properties are always read from jndi.properties (used to be hard-coded)
    git-svn-id: https://svn.novabit.de/nov_nucleus/trunk@479 33553225-e83a-40c5-8946-3093c540bf14

--- bin/import-layouts.cmd
+++ bin/import-layouts.cmd
@@ -4,7 +4,7 @@ setlocal
 set basedir=%~dp0..
 set username=%1
 set password=%2
-set server=%localhost%
+set server=%3
 
 call %basedir%\bin\run-console.cmd %username% %password% %server% -importmasterdatalayouts %basedir%\src\de\novabit\nucleus\client\masterdata\layoutml
 call %basedir%\bin\run-console.cmd %username% %password% %server% -importgenericobjectlayouts %basedir%\src\de\novabit\nucleus\client\genericobject\layoutml
