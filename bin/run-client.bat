4e84bc77712571e07099a40d2a7c35174b07dc41
weber <weber@33553225-e83a-40c5-8946-3093c540bf14>
2009-05-15 11:40:41 +0000

    c building classpath
    git-svn-id: https://svn.novabit.de/nov_nucleus/trunk@2829 33553225-e83a-40c5-8946-3093c540bf14

--- bin/run-client.bat
+++ bin/run-client.bat
@@ -1,55 +1,50 @@
 @echo off
 setlocal
 
-rem set JAVA=P:\Javasoft\JRE\1.5.0.04\bin\javaw
 set JAVA=%JAVA_HOME%\bin\javaw.exe
 
-set NUCLEUS_DIR=%~dp0
-set NUCLEUS_LIBDIR=%NUCLEUS_DIR%\lib
+set CP=%CLASSPATH%;.\conf
 
-set CP=%NUCLEUS_LIBDIR%\nucleus-client.jar
-set CP=%CP%;%NUCLEUS_DIR%\conf
-set CP=%CP%;%NUCLEUS_LIBDIR%\nucleus-xerlin.jar
-set CP=%CP%;%NUCLEUS_LIBDIR%\nucleus-help.jar
-set CP=%CP%;%NUCLEUS_LIBDIR%\novabit-common.jar
-set CP=%CP%;%NUCLEUS_LIBDIR%\log4j-1.2.8.jar
-set CP=%CP%;%NUCLEUS_LIBDIR%\jlfgr-1_0.jar
-set CP=%CP%;%NUCLEUS_LIBDIR%\commons-lang-2.0.jar
-set CP=%CP%;%NUCLEUS_LIBDIR%\commons-beanutils-1.5.jar
-set CP=%CP%;%NUCLEUS_LIBDIR%\commons-collections-3.2.jar
-set CP=%CP%;%NUCLEUS_LIBDIR%\commons-digester-1.3.jar
-set CP=%CP%;%NUCLEUS_LIBDIR%\commons-logging-1.1.jar
+set CP=%CP%;.\lib\nucleus-client.jar
+set CP=%CP%;.\lib\nucleus-xerlin.jar
+set CP=%CP%;.\lib\nucleus-help.jar
+set CP=%CP%;.\lib\novabit-common.jar
+set CP=%CP%;.\lib\log4j-1.2.8.jar
+set CP=%CP%;.\lib\jlfgr-1_0.jar
+set CP=%CP%;.\lib\commons-lang-2.0.jar
+set CP=%CP%;.\lib\commons-beanutils-1.5.jar
+set CP=%CP%;.\lib\commons-collections-3.2.jar
+set CP=%CP%;.\lib\commons-digester-1.3.jar
+set CP=%CP%;.\lib\commons-logging-1.1.jar
 
-set CP=%CP%;%NUCLEUS_LIBDIR%\jboss-j2ee.jar
-set CP=%CP%;%NUCLEUS_LIBDIR%\jboss-client.jar
-set CP=%CP%;%NUCLEUS_LIBDIR%\jboss-common-client.jar
-set CP=%CP%;%NUCLEUS_LIBDIR%\jboss-system-client.jar
-set CP=%CP%;%NUCLEUS_LIBDIR%\jbosssx-client.jar
-set CP=%CP%;%NUCLEUS_LIBDIR%\jboss-transaction-client.jar
-set CP=%CP%;%NUCLEUS_LIBDIR%\jbossmq-client.jar
-set CP=%CP%;%NUCLEUS_LIBDIR%\concurrent.jar
-set CP=%CP%;%NUCLEUS_LIBDIR%\jnp-client.jar
+set CP=%CP%;.\lib\jboss-j2ee.jar
+set CP=%CP%;.\lib\jboss-client.jar
+set CP=%CP%;.\lib\jboss-common-client.jar
+set CP=%CP%;.\lib\jboss-system-client.jar
+set CP=%CP%;.\lib\jbosssx-client.jar
+set CP=%CP%;.\lib\jboss-transaction-client.jar
+set CP=%CP%;.\lib\jbossmq-client.jar
+set CP=%CP%;.\lib\concurrent.jar
+set CP=%CP%;.\lib\jnp-client.jar
 
-set CP=%CP%;%NUCLEUS_LIBDIR%\InsaPerformanceLogger.jar
-rem set CP=%CP%;%NUCLEUS_LIBDIR%\jdbc2_0-stdext.jar
-rem set CP=%CP%;%NUCLEUS_LIBDIR%\jfreechart-0.9.8.jar
-set CP=%CP%;%NUCLEUS_LIBDIR%\jh.jar
-set CP=%CP%;%NUCLEUS_LIBDIR%\StrutLayout1.2.1.jar
-set CP=%CP%;%NUCLEUS_LIBDIR%\thirdparty.jar
-set CP=%CP%;%NUCLEUS_LIBDIR%\webdavlib.jar
-set CP=%CP%;%NUCLEUS_LIBDIR%\dom4j-full.jar
-set CP=%CP%;%NUCLEUS_LIBDIR%\xercesImpl.jar
-set CP=%CP%;%NUCLEUS_LIBDIR%\xmlParserAPIs.jar
-set CP=%CP%;%NUCLEUS_LIBDIR%\davfilechooser.jar
-set CP=%CP%;%NUCLEUS_LIBDIR%\itext-1.3.jar
-set CP=%CP%;%NUCLEUS_LIBDIR%\poi-2.5-final-20040302.jar
-set CP=%CP%;%NUCLEUS_LIBDIR%\jasperreports-3.1.2.jar
-set CP=%CP%;%NUCLEUS_LIBDIR%\jawin.jar
-set CP=%CP%;%NUCLEUS_LIBDIR%\jxl.jar
-set CP=%CP%;%NUCLEUS_LIBDIR%\layer.jar
-set CP=%CP%;%NUCLEUS_LIBDIR%\filters.jar
-set CP=%CP%;%NUCLEUS_LIBDIR%\TableLayout-bin-jdk1.5-2007-04-21.jar
+set CP=%CP%;.\lib\InsaPerformanceLogger.jar
+set CP=%CP%;.\lib\jh.jar
+set CP=%CP%;.\lib\StrutLayout1.2.1.jar
+set CP=%CP%;.\lib\thirdparty.jar
+set CP=%CP%;.\lib\webdavlib.jar
+set CP=%CP%;.\lib\dom4j-full.jar
+set CP=%CP%;.\lib\xercesImpl.jar
+set CP=%CP%;.\lib\xmlParserAPIs.jar
+set CP=%CP%;.\lib\davfilechooser.jar
+set CP=%CP%;.\lib\itext-1.3.jar
+set CP=%CP%;.\lib\poi-2.5-final-20040302.jar
+set CP=%CP%;.\lib\jasperreports-3.1.2.jar
+set CP=%CP%;.\lib\jawin.jar
+set CP=%CP%;.\lib\jxl.jar
+set CP=%CP%;.\lib\layer.jar
+set CP=%CP%;.\lib\filters.jar
+set CP=%CP%;.\lib\TableLayout-bin-jdk1.5-2007-04-21.jar
 
 rem !!!! logging unter c:\temp funktioniert bei Terminalservern nicht !!!!!
 
-start %JAVA% -ea -ms64m -mx192m -cp %CP% de.novabit.nucleus.client.main.Main
+start %JAVA% -ea -ms128m -mx256m -cp %CP% de.novabit.nucleus.client.main.Main
