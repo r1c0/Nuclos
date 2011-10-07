bba838ef6fed2975f39ce41f037c945bfcffd9d7
ramin.goettlich <ramin.goettlich@33553225-e83a-40c5-8946-3093c540bf14>
2011-10-07 16:04:30 +0000

    git-svn-id: https://svn.novabit.de/nov_nucleus/trunk@6969 33553225-e83a-40c5-8946-3093c540bf14

--- nuclos-client/src/main/java/org/nuclos/client/common/NuclosCollectControllerFactory.java
+++ nuclos-client/src/main/java/org/nuclos/client/common/NuclosCollectControllerFactory.java
@@ -117,7 +117,7 @@ public class NuclosCollectControllerFactory {
 			if(sm == null) {
 				JOptionPane.showMessageDialog(
 					parent,
-					CommonLocaleDelegate.getMessage("NuclosCollectControllerFactory.2", "Es ist kein Statusmodell definiert. Das Modul kann nicht geöffnet werden.\nDie Statusmodellverwaltung finden Sie im Menü Administration."),
+					CommonLocaleDelegate.getMessage("NuclosCollectControllerFactory.2", "Es ist kein Statusmodell definiert. Das Modul kann nicht geöffnet werden.\nDie Statusmodellverwaltung finden Sie im Menü Konfiguration."),
 					sEntityName,
 					JOptionPane.WARNING_MESSAGE);
 				return null;
