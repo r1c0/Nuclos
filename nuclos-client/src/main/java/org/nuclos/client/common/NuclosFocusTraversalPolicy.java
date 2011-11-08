b1ab8d95fa109727c30f490884957d17d3f09586
marc.finke <marc.finke@33553225-e83a-40c5-8946-3093c540bf14>
2011-11-08 08:28:04 +0000

    Endlosschleife verhindern, wenn Componente sich selbst als Nachfolger auserkorren hat.
    git-svn-id: https://svn.novabit.de/nov_nucleus/trunk@7104 33553225-e83a-40c5-8946-3093c540bf14

--- nuclos-client/src/main/java/org/nuclos/client/common/NuclosFocusTraversalPolicy.java
+++ nuclos-client/src/main/java/org/nuclos/client/common/NuclosFocusTraversalPolicy.java
@@ -66,6 +66,8 @@ public class NuclosFocusTraversalPolicy extends	LayoutFocusTraversalPolicy {
 			}
 		}
 		Component comp = super.getComponentAfter(aContainer, aComponent);	
+		if(comp.equals(aComponent))
+			return comp;
 		if(comp instanceof JInfoTabbedPane) {
 			JInfoTabbedPane pane = (JInfoTabbedPane)comp;
 			comp = getNextFocusAbleComponent(pane);
