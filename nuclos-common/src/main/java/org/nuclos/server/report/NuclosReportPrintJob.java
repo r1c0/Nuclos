16eee823b8f267eca111c899eab299daf403ad66
stefan.geiling <stefan.geiling@33553225-e83a-40c5-8946-3093c540bf14>
2011-11-24 11:44:46 +0000

    24.11.11 - SG added modifications for "Direktdruck / (Serverseitiger/clientseitiger Druck)"
    git-svn-id: https://svn.novabit.de/nov_nucleus/trunk@7162 33553225-e83a-40c5-8946-3093c540bf14

---
+++ nuclos-common/src/main/java/org/nuclos/server/report/NuclosReportPrintJob.java
@@ -0,0 +1,66 @@
+//Copyright (C) 2010  Novabit Informationssysteme GmbH
+//
+//This file is part of Nuclos.
+//
+//Nuclos is free software: you can redistribute it and/or modify
+//it under the terms of the GNU Affero General Public License as published by
+//the Free Software Foundation, either version 3 of the License, or
+//(at your option) any later version.
+//
+//Nuclos is distributed in the hope that it will be useful,
+//but WITHOUT ANY WARRANTY; without even the implied warranty of
+//MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+//GNU Affero General Public License for more details.
+//
+//You should have received a copy of the GNU Affero General Public License
+//along with Nuclos.  If not, see <http://www.gnu.org/licenses/>.
+package org.nuclos.server.report;
+	
+import java.io.FileInputStream;
+import java.io.Serializable;
+
+import javax.print.DocFlavor;
+import javax.print.DocPrintJob;
+import javax.print.PrintException;
+import javax.print.PrintService;
+import javax.print.SimpleDoc;
+import javax.print.attribute.PrintRequestAttributeSet;
+
+import org.apache.log4j.Logger;
+
+/**
+ *
+ * @author 
+ */
+public abstract class NuclosReportPrintJob implements Serializable {
+
+	private final static Logger log = Logger.getLogger(NuclosReportPrintJob.class);
+	
+	public static synchronized void checkJawin() throws NuclosReportException {
+		if (!jawinLoaded) {
+			try {
+				System.loadLibrary("jawin");
+				jawinLoaded = true;
+			} catch (SecurityException e) {
+				log.error("Error loading jawin", e);
+				throw new NuclosReportException("nuclos.jawin.linkerror", e);
+			} catch (UnsatisfiedLinkError e) {
+				log.error("Error loading jawin", e);
+				throw new NuclosReportException("nuclos.jawin.linkerror", e);
+			}
+		}
+	}
+
+	private static volatile boolean jawinLoaded = false;
+	
+	public void print(PrintService prserv, String sFilename,
+			PrintRequestAttributeSet aset) throws PrintException {
+		try {
+			DocPrintJob pj = prserv.createPrintJob();
+	        FileInputStream fis = new FileInputStream(sFilename);
+	        pj.print(new SimpleDoc(fis, DocFlavor.INPUT_STREAM.AUTOSENSE, null), (PrintRequestAttributeSet) aset);
+		} catch (Exception e) {
+			throw new PrintException(e.getMessage());
+		}
+	}
+}
