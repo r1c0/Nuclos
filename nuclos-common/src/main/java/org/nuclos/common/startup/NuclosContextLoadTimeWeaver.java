7ac27e8a346ac3f8ed606c7da3b3c8a2e6322627
thomas.pasch <thomas.pasch@33553225-e83a-40c5-8946-3093c540bf14>
2012-04-19 13:32:39 +0000

    Added AspectJ Load-time weaving support for use with eclipse.
    Signed-off-by: Thomas Pasch <thomas.pasch@gmx.de>
    
    (cherry picked from commit c3f708da0f95eff8156d53eccc25c6d00bc77241)
    
    Signed-off-by: Thomas Pasch <thomas.pasch@gmx.de>
    
    git-svn-id: https://svn.novabit.de/nov_nucleus/trunk@7950 33553225-e83a-40c5-8946-3093c540bf14

---
+++ nuclos-common/src/main/java/org/nuclos/common/startup/NuclosContextLoadTimeWeaver.java
@@ -0,0 +1,89 @@
+package org.nuclos.common.startup;
+
+import java.lang.instrument.ClassFileTransformer;
+import java.lang.management.ManagementFactory;
+import java.lang.management.RuntimeMXBean;
+import java.util.List;
+
+import org.apache.log4j.Logger;
+import org.springframework.context.weaving.DefaultContextLoadTimeWeaver;
+import org.springframework.instrument.classloading.LoadTimeWeaver;
+import org.springframework.instrument.classloading.SimpleThrowawayClassLoader;
+
+public class NuclosContextLoadTimeWeaver extends DefaultContextLoadTimeWeaver {
+	
+	private static final Logger LOG = Logger.getLogger(NuclosContextLoadTimeWeaver.class); 
+	
+	public NuclosContextLoadTimeWeaver() {		
+	}
+
+	@Override
+	public void setBeanClassLoader(ClassLoader classLoader) {
+		if (isLtw(classLoader)) {
+			super.setBeanClassLoader(classLoader);
+		}
+		else {
+			super.setBeanClassLoader(new DummyInstrumentedClassLoader(classLoader));
+		}
+	}
+	
+	private boolean isLtw(ClassLoader classLoader) {
+		boolean result = false;
+		final String name = classLoader.getClass().getName();
+		if (name.indexOf("InstrumentableClassLoader") >= 0) {
+            LOG.info("Found instrumentable class loader: " + name);
+			result = true;
+		}
+        else {
+            RuntimeMXBean RuntimemxBean = ManagementFactory.getRuntimeMXBean();
+            List<String> arguments = RuntimemxBean.getInputArguments();
+            for (String s: arguments) {
+                    if (s != null && s.indexOf("-javaagent:") >= 0) {
+                            LOG.info("Found javaagent vm arg: " + s);
+                            result = true;
+                            break;
+                    }
+            }
+        }
+		LOG.info("enable LTW: " + result);
+		return result;
+	}
+	
+	public static final class DummyInstrumentedClassLoader extends ClassLoader {
+		
+		private DummyInstrumentedClassLoader(ClassLoader parent) {
+			super(parent);
+		}
+		
+		public void addTransformer(ClassFileTransformer trans) {
+			// ignore
+		}
+		
+	}
+	
+	public final static class DummyLoadTimeWeaver implements LoadTimeWeaver {
+		
+		private final ClassLoader classLoader;
+		
+		private DummyLoadTimeWeaver(ClassLoader classLoader) {
+			this.classLoader = classLoader;
+		}
+
+		@Override
+		public void addTransformer(ClassFileTransformer transformer) {
+			// ignore
+		}
+
+		@Override
+		public ClassLoader getInstrumentableClassLoader() {
+			return classLoader;
+		}
+
+		@Override
+		public ClassLoader getThrowawayClassLoader() {
+			return new SimpleThrowawayClassLoader(classLoader);
+		}
+		
+	}
+	
+}
