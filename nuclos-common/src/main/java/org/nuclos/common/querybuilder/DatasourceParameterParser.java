aafe8b26c4f34eabbe997f0c197f542de6ab5fb0
thomas.pasch <thomas.pasch@33553225-e83a-40c5-8946-3093c540bf14>
2012-05-22 14:46:16 +0000

    Fixed DS parameters that are prefixes of other DS parameters.
    Signed-off-by: Thomas Pasch <thomas.pasch@gmx.de>
    
    (cherry picked from commit 9f0e760eaef82819de194863a3decbb15917e559)
    
    Signed-off-by: Thomas Pasch <thomas.pasch@gmx.de>
    
    git-svn-id: https://svn.novabit.de/nov_nucleus/trunk@8184 33553225-e83a-40c5-8946-3093c540bf14

---
+++ nuclos-common/src/main/java/org/nuclos/common/querybuilder/DatasourceParameterParser.java
@@ -0,0 +1,26 @@
+package org.nuclos.common.querybuilder;
+
+import java.util.Iterator;
+import java.util.regex.Pattern;
+
+import org.nuclos.common.dblayer.FieldRefIterator;
+import org.nuclos.common.dblayer.IFieldRef;
+
+public class DatasourceParameterParser implements Iterable<IFieldRef> {
+	
+	private static final Pattern PARAM_PATTERN = Pattern.compile("\\$(\\p{Alpha}[\\p{Alnum}_]*)", Pattern.MULTILINE);
+	
+	//
+	
+	private final String sql;
+	
+	public DatasourceParameterParser(String sql) {
+		this.sql = sql;
+	}
+
+	@Override
+	public Iterator<IFieldRef> iterator() {
+		return new FieldRefIterator(PARAM_PATTERN, sql);
+	}
+
+}
