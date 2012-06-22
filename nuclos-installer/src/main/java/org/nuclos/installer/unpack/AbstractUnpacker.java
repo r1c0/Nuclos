1342bb5e5b7a21ca6a06ab6857cfb3794aa2c3a3
maik.stueker <maik.stueker@33553225-e83a-40c5-8946-3093c540bf14>
2012-06-22 11:29:52 +0000

    git-svn-id: https://svn.novabit.de/nov_nucleus/trunk@8405 33553225-e83a-40c5-8946-3093c540bf14

--- nuclos-installer/src/main/java/org/nuclos/installer/unpack/AbstractUnpacker.java
+++ nuclos-installer/src/main/java/org/nuclos/installer/unpack/AbstractUnpacker.java
@@ -363,7 +363,7 @@ public abstract class AbstractUnpacker implements Unpacker, Constants {
 
 			// update check (remove files that were not unpacked within the current installation
 			for (String file : FileUtils.getFiles(nuclosHome,
-					new File((String) ConfigContext.getCurrentConfig().get("nuclet.conf.path")),
+					new File(ConfigContext.getFileProperty("server.conf.dir"), "nuclet"),
 					new File(nuclosHome, "data"),
 					new File(nuclosHome, "logs"),
 					new File(ConfigContext.getFileProperty("server.tomcat.dir"), "logs"))) {
