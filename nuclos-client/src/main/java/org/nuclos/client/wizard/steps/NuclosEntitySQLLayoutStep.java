a1d1dddcb8038b30868a1ad3a3950018cc3e3203
stefan.geiling <stefan.geiling@33553225-e83a-40c5-8946-3093c540bf14>
2012-07-19 15:26:02 +0000

    refs NUCLOSINT-1619
    git-svn-id: https://svn.novabit.de/nov_nucleus/branches/prod/3.5@8608 33553225-e83a-40c5-8946-3093c540bf14

--- nuclos-client/src/main/java/org/nuclos/client/wizard/steps/NuclosEntitySQLLayoutStep.java
+++ nuclos-client/src/main/java/org/nuclos/client/wizard/steps/NuclosEntitySQLLayoutStep.java
@@ -1154,8 +1154,7 @@ public class NuclosEntitySQLLayoutStep extends NuclosEntityAbstractStep {
 
 			 final List<EntityFieldMetaDataVO> metaFields = new ArrayList<EntityFieldMetaDataVO>(MetaDataDelegate.getInstance().getAllEntityFieldsByEntity(wizardModel.getEntityName()).values());
 
-			 // @see NUCLOS-573
-			 /*final List<EntityFieldMetaDataVO> fields = new ArrayList<EntityFieldMetaDataVO>();
+			 final List<EntityFieldMetaDataVO> fields = new ArrayList<EntityFieldMetaDataVO>();
 
 			 for(int i = 0; i < listAttributeOrder.getModel().getSize(); i++) {
 				 Attribute attr = (Attribute)listAttributeOrder.getModel().getElementAt(i);
@@ -1165,7 +1164,13 @@ public class NuclosEntitySQLLayoutStep extends NuclosEntityAbstractStep {
 						 break;
 					 }
 				 }
-			 }*/
+			 }
+			 // add system fields.
+			 for(EntityFieldMetaDataVO voField : metaFields) {
+				 if (!fields.contains(voField))
+					 fields.add(voField);
+			 }
+			 
 			 
 			 final Map<Long, String> attributeGroups = new HashMap<Long, String>();
 			 for(String sGroup : getAttributeGroups()) {
