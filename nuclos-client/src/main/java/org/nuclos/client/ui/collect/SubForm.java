abbc028d298cc7287db09411297ca0854711d0ca
stefan.geiling <stefan.geiling@33553225-e83a-40c5-8946-3093c540bf14>
2012-07-19 15:39:15 +0000

    19.07.12 - SG / nullpointer fix.
    git-svn-id: https://svn.novabit.de/nov_nucleus/branches/prod/3.5@8609 33553225-e83a-40c5-8946-3093c540bf14

--- nuclos-client/src/main/java/org/nuclos/client/ui/collect/SubForm.java
+++ nuclos-client/src/main/java/org/nuclos/client/ui/collect/SubForm.java
@@ -2177,6 +2177,11 @@ public class SubForm extends JPanel
 			} else if (event instanceof MouseEvent)
             	newRowOnNext = false;
 			
+			if (getSubForm() == null) {
+				super.changeSelection(rowIndex, columnIndex, toggle, extend);
+				return;
+			}
+			
 			String sNextColumn = getSelectedColumn() == -1 ? null : getSubForm().getColumnNextFocusComponent((String)getColumnModel().getColumn(getSelectedColumn()).getIdentifier());
 			if (sNextColumn != null) {
 				int colIndex = sNextColumn == null ? -1 : getColumnModel().getColumnIndex(sNextColumn);
