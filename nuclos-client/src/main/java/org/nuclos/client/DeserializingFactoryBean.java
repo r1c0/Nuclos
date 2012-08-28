c618de6fb82cfcab745d7dedb29279662c8da430
thomas.pasch <thomas.pasch@33553225-e83a-40c5-8946-3093c540bf14>
2012-08-28 08:06:02 +0000

    Another round of NUCLOS-1049 fixing.
    Signed-off-by: Thomas Pasch <thomas.pasch@gmx.de>
    
    (cherry picked from commit dc1dd0015da03bc0c772abab8ae72eeab6474b32)
    
    Signed-off-by: Thomas Pasch <thomas.pasch@gmx.de>
    
    git-svn-id: https://svn.novabit.de/nov_nucleus/trunk@8852 33553225-e83a-40c5-8946-3093c540bf14

--- nuclos-client/src/main/java/org/nuclos/client/DeserializingFactoryBean.java
+++ nuclos-client/src/main/java/org/nuclos/client/DeserializingFactoryBean.java
@@ -1,18 +1,28 @@
 package org.nuclos.client;
 
+import org.apache.log4j.Logger;
 import org.springframework.beans.BeanUtils;
 
 public class DeserializingFactoryBean {
 	
+	private final static Logger LOG = Logger.getLogger(DeserializingFactoryBean.class);
+	
     public DeserializingFactoryBean() {
     }
     
-    public void afterPropertiesSet() throws Exception {
-    }
-    
-    public Object createInstance(Class clazz) throws Exception {
-    	Object object = LocalUserCaches.getInstance().getObject(clazz);
-    	return (object != null) ? object : BeanUtils.instantiateClass(clazz);
+    public Object createInstance(Class<?> clazz) {
+    	try {
+    		Object object = LocalUserCaches.getInstance().getObject(clazz);
+    		return (object != null) ? object : BeanUtils.instantiateClass(clazz);
+    	}
+    	/*
+    	 * If something goes wrong here (i.e. NPE in deserialize(clazz)), just
+    	 * ignore the serialized stuff. (tp)
+    	 */
+    	catch (Exception e) {
+    		LOG.warn("Client caches: recreating bean for " + clazz.getName() + " failed: " + e, e);
+    		return BeanUtils.instantiateClass(clazz);
+    	}
     }
 }
 
