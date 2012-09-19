5282184c5f5d5109639a57fad2801e61d63a1f21
stefan.geiling <stefan.geiling@33553225-e83a-40c5-8946-3093c540bf14>
2012-09-19 14:24:25 +0000

    refs NUCLOS-1124
    git-svn-id: https://svn.novabit.de/nov_nucleus/trunk@9022 33553225-e83a-40c5-8946-3093c540bf14

--- nuclos-client/src/main/java/org/nuclos/client/genericobject/GenericObjectCollectController.java
+++ nuclos-client/src/main/java/org/nuclos/client/genericobject/GenericObjectCollectController.java
@@ -2352,7 +2352,11 @@ public class GenericObjectCollectController extends EntityCollectController<Coll
 				// iterate over the models rather than over the components:
 				final CollectableComponentModel clctcompmodel = layoutrootDetails.getCollectableComponentModelFor(sFieldName);
 				final CollectableField clctfShown = clct.getField(sFieldName);
-				clctcompmodel.setFieldInitial(clctfShown, !clctfShown.isNull()); //@see NUCLOS-1065
+				if (clctcompmodel.getEntityField().isReferencing()) { //@see NUCLOS-1065
+					clctcompmodel.setFieldInitial(clctfShown, !clctfShown.isNull()); 
+				} else {
+					clctcompmodel.setFieldInitial(clctfShown, true);
+				}
 
 				markFieldInHistoricalView(lowdCurrent, sFieldName, clctfShown);
 			}
